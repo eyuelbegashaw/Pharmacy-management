6f2c51bbf57a8027cf164b4e660607e1c54fd1d7
eyuel begashaw <eyuelbegashaw214@gmail.com>
2022-12-21 22:10:25 +0300

    error middleware

---
+++ backend/middlewares/errorMiddleware.js
@@ -0,0 +1,12 @@
+export const NotFound = (req, res, next) => {
+  res.status(404).json("Page Not Found");
+};
+
+export const ErrorMiddleware = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
